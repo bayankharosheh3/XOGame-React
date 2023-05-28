88ac3df79f3e0a021a04558ba36588c5294f772a
bayankharosheh3 <bayankharoseh3@gmail.com>
2023-05-28 09:52:08 +0300

    routing

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <>
+      <div>NotFound</div>
+      <Link to='/XOGame-React'>main</Link>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
